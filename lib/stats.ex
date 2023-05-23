9df321497fc10c3b206eef3d83bdb2568a7c8599
hillaryomondi <hillary.okoth@strathmore.edu>
2023-05-23 10:04:01 +0300

    Added sample and population function delegates to Stats file

--- lib/stats.ex
+++ lib/stats.ex
@@ -1,18 +1,9 @@
 defmodule Stats do
-  @moduledoc """
-  Documentation for Stats.
-  """
+  alias Stats.CentralTendency.Mean
 
-  @doc """
-  Hello world.
+  #def population_mean(nums), do: Mean.population_mean(nums)
+  defdelegate population_mean(nums), to: Mean
 
-  ## Examples
+  defdelegate sample_mean(nums), to: Mean
 
-      iex> Stats.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
